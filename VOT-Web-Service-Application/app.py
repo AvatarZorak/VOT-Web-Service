62b08c91a37caeefab0875725d31b871f31eaa84
AvatarZorak <alexhristov2005@gmail.com>
2024-06-19 12:11:21 +0300

    Almost finished version

--- VOT-Web-Service-Application/app.py
+++ VOT-Web-Service-Application/app.py
@@ -1,32 +1,83 @@
-from flask import Flask, redirect, url_for, render_template, request, session, jsonify
-from flask_oidc import OpenIDConnect
+import os
+
+from flask import Flask, redirect, url_for, render_template, request, session, jsonify, g, send_from_directory
 import requests
 from flask_cors import CORS
+import mysql.connector
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app, origins=['*'])
 
-app.config['SECRET_KEY'] = 'v519YWstZmoWqRGZGxYFFEYUr9bDLSP2'
+app.config['CLIENT_SECRET'] = 'ehAfjucEgNpS3ZujbcVjecxI31wmtBWZ'
+app.config['CLIENT_ID'] = 'back-end-client'
+app.config['UPLOADS_FOLDER'] = './'
+app.config['DB_USER'] = 'maxscale'
+app.config['DB_PASSWORD'] = 'mariadb'
+app.config['DB_HOST'] = 'localhost'
+app.config['DB_PORT'] = 4006
+app.config['DB_NAME'] = 'application'
 
-app.config['OIDC_CLIENT_SECRETS'] = 'client_secrets.json'
-app.config['OIDC_SCOPES'] = ['openid', 'email', 'profile']
 
-oidc = OpenIDConnect(app)
+cnx = mysql.connector.connect(
+    user=app.config['DB_USER'],
+    password=app.config['DB_PASSWORD'],
+    host=app.config['DB_HOST'],
+    port=app.config['DB_PORT'],
+    database=app.config['DB_NAME']
+)
+
+
+def get_user_id(access_token):
+    keycloak_response = requests.post(
+        url='http://localhost:8080/realms/application/protocol/openid-connect/token/introspect',
+        data={'token': f'{access_token}',
+              'client_id': f'{app.config["CLIENT_ID"]}',
+              'client_secret': f'{app.config["CLIENT_SECRET"]}'
+              }
+    )
+
+    return keycloak_response.json()['name']
 
 
 @app.route('/', methods=['POST'])
-@oidc.require_login
 def hello_world():
-    if oidc.user_loggedin:
-        return jsonify(oidc.user_getinfo(["email"]))
+    user_id = get_user_id(request.headers.get('Authorization'))
+
+    return user_id, 200
+
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    user_id = get_user_id(request.headers.get('Authorization'))
+    file = request.files['image']
+
+    cursor = cnx.cursor()
+
+    cursor.execute("SELECT count(filename), JSON_ARRAY(filename) FROM images WHERE user_id=%s GROUP BY user_id;", (user_id, ))
+
+    results = cursor.fetchone()
+    # count = results[0]
+    # filenames = results[1]
+
+    print(results)
+
+    # print(count)
+    # print(filenames)
+
+    if results is None:
+        count = 0
+        filenames = []
     else:
-        return redirect(url_for('oidc_auth.login'))
-    # return "hello world"
+        count = results[0]
+        filenames = results[1]
 
+    new_file_format = secure_filename(file.filename).split('.')[-1]
+    new_filename = f'{user_id}-{count}.{new_file_format}'
 
-@app.route('/callback')
-def callback():
-    return redirect(url_for('/'))
+    file.save(os.path.join(app.config['UPLOADS_FOLDER'], new_filename))
+
+    return send_from_directory(app.config['UPLOADS_FOLDER'], new_filename)
 
 
 if __name__ == '__main__':
--- front-end-project/src/App.js
+++ front-end-project/src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import Keycloak from "keycloak-js";
 import {useEffect, useState} from 'react';
 import axios from 'axios';
 
+import Keycloak from "keycloak-js";
+
 let keycloak_options = {
   url: 'http://localhost:8080',
-  realm: 'master',
-  clientId: 'vot-web-service'
+  realm: 'application',
+  clientId: 'front-end-client'
 }
+
+console.log("1")
 
 let keycloak = new Keycloak(keycloak_options)
 
-keycloak.init({
+await keycloak.init({
   onLoad: "login-required",
   checkLoginIframe: false,
   pkceMethod: "S256"
@@ -26,23 +29,78 @@ keycloak.init({
   console.log("Authentication error")
   console.log(keycloak)
 })
+//
+// const body = {
+//   message: 'something'
+// };
+//
+// const headers = {
+//     'Content-Type': 'application/json',
+//     'Authorization': `${keycloak.token}`
+// };
+//
+// let responseBody;
+//
+// await axios.post("http://localhost:5000/", body, { headers }).then(response => {
+//     responseBody = response.data;
+// });
 
 function App() {
-  const [responseBody, setResponseBody] = useState([{}])
 
-  useEffect(() => {
-    axios.post("http://localhost:5000/", {
-      headers: {
-        "Authorization": `Bearer ${keycloak.token}`
-      }
-    }).then(response => {
-      setResponseBody(response.data);
-    })
-  }, [])
+  const [selectedImage, setSelectedImage] = useState(null)
+  const [imageURL, setImageURL] = useState(null)
+
+  const fileChange = (event) => {
+    const image = event.target.files[0]
+    const allowedTypes = ['image/png', 'image/jpg', 'image/jpeg']
+
+    if(image && allowedTypes.includes(image.type)) {
+      setSelectedImage(image);
+    } else {
+      setSelectedImage(null);
+      console.log('ERROR! INVALID IMAGE TYPE!');
+    }
+
+  };
+
+  const fileUpload = async () => {
+    console.log("sending")
+
+    if(selectedImage) {
+      console.log("in")
+      const formData = new FormData();
+      formData.append("image", selectedImage);
+
+      await axios.post("http://localhost:5000/upload", formData, {
+        responseType: "blob",
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Authorization': `${keycloak.token}`
+        }
+      }).then(response => {
+        const blobURL = window.URL.createObjectURL(new Blob([response.data]))
+        console.log(blobURL);
+        setImageURL(blobURL);
+        setSelectedImage(null);
+      }).catch(error => {
+        console.log(error)
+      });
+    } else {
+      console.log("out")
+    }
+  }
+
 
   return (
     <>
-      Hello, {responseBody.body}
+        <input type="file" onChange={fileChange} />
+        <br/>
+        <button onClick={fileUpload}>Upload</button>
+        {imageURL ? (
+          <img src={imageURL} alt="Fetched content" />
+        ) : (
+          <p>Loading...</p>
+        )}
     </>
   );
 }
